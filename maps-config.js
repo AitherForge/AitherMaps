711c2d72e02250e4b828e3c8392c856898ad33b4
DJTheBest <djthebest53@gmail.com>
2026-09-10 06:43:37 -0500

    Add Google Maps Demo Key configuration

---
+++ maps-config.js
@@ -0,0 +1,4 @@
+// Aither Maps Google Maps Demo Key configuration.
+// Get a free Maps Demo Key from Google Maps Platform and paste it below.
+// This key is intended for prototyping/testing and should be restricted when possible.
+window.AITHER_MAPS_DEMO_KEY = 'PASTE_YOUR_GOOGLE_MAPS_DEMO_KEY_HERE';
